691fc5f1b794552f1259bfe69fa66121b983c735
Hector <hectorscode@gmail.com>
2020-10-18 23:37:57 -0700

    Setup initial app page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// function Home() {
+//   return <div>SRE To-do List</div>;
+// }
+
+// refactor above function with arrow function
+const Home = () => <div>SRE To-do List</div>;
+
+
+export default Home;
